9de087d7a7cca481afd54f1ab8e6614de71d0d26
javierConpec <123789471+javierConpec@users.noreply.github.com>
2024-06-17 21:03:28 -0600

    update- javier

--- src/models/Paquete.ts
+++ src/models/Paquete.ts
@@ -2,6 +2,7 @@ export interface IPaquete{
     idPaquete: number;
     nombre: string;
     descripcion: string;
+    descuento: number;
     estadoAuditoria:string;
     fechaCreacion: Date;
     fechaModificacion: Date;
--- src/routes/rolRoutes.ts
+++ src/routes/rolRoutes.ts
@@ -7,11 +7,11 @@ router.post('/',insertarRol);
 router.get('/',listarRoles);
 router.get('/:id',obtenerRol);
 router.put('/:id',modificarRol);
-<<<<<<< HEAD
+
 router.patch('/:id',eliminarRol);
 
-=======
+
 router.delete('/:id',eliminarRol);
 //prueba
->>>>>>> 130b4a0ac54fec2287840987a3fd9eb2d010063b
+
 export default router;
