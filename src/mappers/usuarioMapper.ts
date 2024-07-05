be365fbfcd07d90cdc020605d9b486cf12b38445
javierConpec <123789471+javierConpec@users.noreply.github.com>
2024-07-05 16:27:32 -0600

    .

--- src/mappers/usuarioMapper.ts
+++ src/mappers/usuarioMapper.ts
@@ -5,6 +5,28 @@ import { fromPrismaRol } from "./rolMapper";
 export const fromPrismaUsuario = (usuario: usuarios, rol:roles): any =>({
     idUsuario:usuario.id_usuario,
     rol:fromPrismaRol(rol),
-    nombre:usuario.nombre,
+    nombres:usuario.nombres,
+    apellidoPaterno:usuario.apellido_paterno,
+    apellidoMaterno:usuario.apellido_materno,
+    userName:usuario.username,
+    email:usuario.email,
+    telefono:usuario.telefono,
+    direccion:usuario.direccion,
+    sexo:usuario.sexo,
+    fechaCreacion:usuario.fecha_creacion
     
+});
+
+export const toPrismaUsuario = (usuario:IUsuario):any=>({
+    id_usario:usuario.idUsuario,
+    id_rol:usuario.rol.idRol,
+    nombres:usuario.nombres,
+    apellido_Paterno:usuario.apellidoPaterno,
+    apellido_Materno:usuario.apellidoMaterno,
+    username:usuario.username,
+    email:usuario.email,
+    telefono:usuario.telefono,
+    direccion:usuario.direccion
+
 })
+ 
--- src/services/usuarioService.ts
+++ src/services/usuarioService.ts
@@ -1,4 +1,13 @@
 import { PrismaClient, usuarios } from "@prisma/client";
 import { IUsuario } from "../models/usuario";
 import { RESPONSE_DELETE_OK, RESPONSE_INSERT_OK,RESPONSE_UPDATE_OK } from "../shared/constants";
-import {}
+import { fromPrismaUsuario,toPrismaUsuario } from "../mappers/usuarioMapper";
+
+const prisma = new  PrismaClient();
+
+export const insertarUsuario = async(usuario:IUsuario)=>{
+    await prisma.usuario.create({
+        data:toPrismaUsuario(usuario)
+    });
+    return RESPONSE_INSERT_OK
+}
