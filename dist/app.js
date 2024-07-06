f5585695442e40f214c5d18596eb0572cc9cbb53
javierConpec <123789471+javierConpec@users.noreply.github.com>
2024-07-06 04:08:07 -0600

    .

--- dist/app.js
+++ dist/app.js
@@ -11,13 +11,25 @@ const rolRoutes_1 = __importDefault(require("./routes/rolRoutes"));
 const paisRoutes_1 = __importDefault(require("./routes/paisRoutes"));
 const categoriaRoutes_1 = __importDefault(require("./routes/categoriaRoutes"));
 const paqueteRoutes_1 = __importDefault(require("./routes/paqueteRoutes"));
+const usuarioRoutes_1 = __importDefault(require("./routes/usuarioRoutes"));
+const destinoRoutes_1 = __importDefault(require("./routes/destinoRoutes"));
+const hospedajeRoutes_1 = __importDefault(require("./routes/hospedajeRoutes"));
+const viajeRouter_1 = __importDefault(require("./routes/viajeRouter"));
+const itinerarioRoutes_1 = __importDefault(require("./routes/itinerarioRoutes"));
+const reservaRoutes_1 = __importDefault(require("./routes/reservaRoutes"));
 const app = (0, express_1.default)();
 //Midlewares
 app.use(express_1.default.json());
 //Routes
-app.use('/api/v1.0/roles', rolRoutes_1.default);
+app.use('/api/v1.0/rol', rolRoutes_1.default);
 app.use('/api/v1.0/pais', paisRoutes_1.default);
-app.use('/api/v1.0/categorias', categoriaRoutes_1.default);
-app.use('/api/v1.0/paquetes', paqueteRoutes_1.default);
+app.use('/api/v1.0/categoria', categoriaRoutes_1.default);
+app.use('/api/v1.0/paquete', paqueteRoutes_1.default);
+app.use('/api/v1.0/usuario', usuarioRoutes_1.default);
+app.use('/api/v1.0/destino', destinoRoutes_1.default);
+app.use('/api/v1.0/hospedaje', hospedajeRoutes_1.default);
+app.use('/api/v1.0/viaje', viajeRouter_1.default);
+app.use('/api/v1.0/itinerario', itinerarioRoutes_1.default);
+app.use('/api/v1.0/itinerario', reservaRoutes_1.default);
 exports.default = app;
 //# sourceMappingURL=app.js.map
